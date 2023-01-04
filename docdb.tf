e9cef329d3e202c4809de84120f606d7f8d3b5b1
Raghu K <raghuk.vit@gmail.com>
2023-01-04 08:22:39 +0530

    Update tf-modules/tf-module-docdb/

--- docdb.tf
+++ docdb.tf
@@ -1,9 +1,21 @@
-//resource "aws_docdb_cluster" "docdb" {
-//  cluster_identifier      = "${var.env}-docdb"
-//  engine                  = var.engine
-//  master_username         = data.aws_ssm_parameter.username.value
-//  master_password         = data.aws_ssm_parameter.password.value
-//  backup_retention_period = var.backup_retention_period
-//  preferred_backup_window = var.preferred_backup_window
-//  skip_final_snapshot     = var.skip_final_snapshot
-//}
+resource "aws_docdb_subnet_group" "docdb" {
+  name       = "${var.env}-docdb"
+  subnet_ids = local.app_subnets_ids
+
+  tags = {
+    Name = "${var.env}-docdb"
+  }
+}
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.env}-docdb"
+  engine                  = var.engine
+  master_username         = data.aws_ssm_parameter.username.value
+  master_password         = data.aws_ssm_parameter.password.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
+  kms_key_id              = var.kms_key_id
+  vpc_security_group_ids  = [aws_security_group.allow_docdb.id]
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+}
--- vars.tf
+++ vars.tf
@@ -4,3 +4,4 @@ variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
 variable "vpc" {}
+variable "kms_key_id" {}
